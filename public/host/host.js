2fdbed1f1d9ea8aef03546f6a9992f3f001cdbec
Flo <Florian.Lukas.Unger@gmail.com>
2026-02-01 22:05:34 +0100

    finished home screen started backend impementation

--- public/host/host.js
+++ public/host/host.js
@@ -1,21 +1,40 @@
 const modal = document.getElementById("modal");
 const input = document.getElementById("name-input");
+const notEnough = document.getElementById("not-enough");
+const okButton = document.getElementById("button-ok");
 const names = [];
 
 document.getElementById("add-btn").onclick = () => {
     modal.style.display = "flex";
+    input.style.display = "flex";
     input.focus();
 };
+
+document.getElementById("start-btn").onclick = () => {
+    if(names.length <= 1){
+        modal.style.display = "flex";
+        notEnough.style.display = "flex";
+    }else{
+        //start game
+    }
+}
+
+okButton.onclick = () => {
+    modal.style.display = "none";
+    notEnough.style.display = "none";
+}
 
 input.addEventListener("keydown", (e) => {
     if(e.key === "Escape"){
         input.value = "";
         modal.style.display = "none";
+        input.style.display = "none";
     }
     if(e.key === "Enter" && input.value !== ""){
         names.push(input.value);
         input.value = "";
         modal.style.display = "none";
+        input.style.display = "none";
         updateList();
     }
 });
